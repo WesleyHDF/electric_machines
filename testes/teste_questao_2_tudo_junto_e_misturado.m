9acaf451c8f4059355b901f9b0cfedfa2908366e
Miguel-mmf <miguel.ferreira@cear.ufpb.br>
2022-11-02 11:50:58 -0300

    quiver vector plot

--- testes/teste_questao_2_tudo_junto_e_misturado.m
+++ testes/teste_questao_2_tudo_junto_e_misturado.m
@@ -1,8 +1,6 @@
 clc
 clear
-
 addpath 'E:\Repositories Github\electric_machines\utils'
-%addpath 'E:\Repositories Github\electric_machines\q02'
 
 %% Variáveis de Entrada
 %vt = 13800; % tensão de terminal do GS
@@ -20,11 +18,20 @@ freq_ele = 60; % frequência elétrica em Hz.
 If_x_Vt = readtable('../caracteristica_a_vazio_gs.csv');
 s = size(If_x_Vt.VT);
 
-for c = 8:s(1)
-    %disp(c)
+figure;
+plot(If_x_Vt.IF,If_x_Vt.VT,"Color","b",'LineWidth',2);
+grid on;
+xlabel('Corrente de Campo (A)','Interpreter','latex');
+ylabel('Tensão de Terminal (V)','Interpreter','latex');
+title('Caracteristica a Vazio do Gerador Síncrono','Interpreter','latex');
 
-    Vt = If_x_Vt(c,:).VT;
-    %disp(Vt)
+Eax_test = zeros(s(1)-8,1);
+Eay_test = zeros(s(1)-8,1);
+
+figure;
+for c = 8:s(1)
+
+    Vt = If_x_Vt(c,:).VT; % tensão de terminal com base na curva caracterísitca a vazio do GS
 
     %% Cálculo - GS operando Nominalmente
     [Ia_ref,Il_ref,Ea_ref,Z_ref,Fp_ref,Vel_rpm] = calc_op_nominal_gs( ...
@@ -46,6 +53,9 @@ for c = 8:s(1)
     [Ea,jXsIa,Z] = calc_tensao_induzida(Vp,0,Ia,Ia_phase,Xs,Ra); % angulo de fase de Vp é 0
     disp(abs(Ea))
 
+    Eax_test(c-7) = abs(Ea);
+    Eay_test(c-7) = imag(Ea);
+
     %% Cálculo de Ia, Il, Ea e Fp em pu
     % tá funcionando
 %     [Ia_pu,Il_pu,Ea_pu,Z_pu,Fp_pu] = calc_gs_pu( ...
@@ -64,11 +74,25 @@ for c = 8:s(1)
     fp = acos((Ea*sind(0-angle(Ea)))/(abs(Z)*Ia));
     
     %% Gráficos
-    compass(abs(Ea),imag(Ea),'-.','g');
+    %compass(abs(Ea),imag(Ea),'-.','g');
+    quiver(0*abs(Ea),0*imag(Ea),abs(Ea),imag(Ea),0,'g');
     hold on;
-    compass(Vp,0,'b');
-    compass(Ia*cosd(Ia_phase),Ia*sind(Ia_phase),'r');
-    compass(abs(jXsIa),imag(jXsIa),'m');
+    quiver(0*Vp,0*Vp,Vp,0*Vp,0,'b');
+    quiver(0*Vp,0*Vp,Ia*cosd(Ia_phase),Ia*sind(Ia_phase),0,'r');
+    quiver(Vp,0*Vp,abs(jXsIa),abs(jXsIa),0,'m');
+    %compass(Vp,0,'b');
+    %compass(Ia*cosd(Ia_phase),Ia*sind(Ia_phase),'r');
+    %compass(abs(jXsIa),imag(jXsIa),'m');
     %compass(Ea,'-.','r')
 
+    %break
+
 end
+
+%fig2plotly('TreatAs', 'compass');
+
+% figure;
+% disp(zeros(s(1)-7,1));
+% disp(Eax_test);
+% disp(Eay_test);
+% quiver(0*Eax_test,0*Eay_test,Eax_test,Eay_test,0);
